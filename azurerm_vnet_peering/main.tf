2d3b7bfb7fbbff70fab9ba456cf951ecc5b9ce65
veenuindolia <veenuindolia@gmail.com>
2026-06-12 16:50:49 +0530

    added 7 resources code

---
+++ azurerm_vnet_peering/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_virtual_network_peering" "vnet_peering" {
+  for_each                  = var.peerings
+  name                      = each.value.name
+  resource_group_name       = each.value.resource_group_name
+  virtual_network_name      = each.value.virtual_network_name
+  remote_virtual_network_id = each.value.remote_virtual_network_id
+}
